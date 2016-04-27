f114c3b837097b9c3d0bcb28824cc1416fed4d7a
Dominique Busser <dominique.busser@netlight.com>
2016-04-27 12:16:55 +0200

    Fix report settings

--- test/Time.spec.js
+++ test/Time.spec.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import { expect } from 'chai';
-import moment from 'moment';
 
 import Time from '../src/components/time/Time.jsx';
 
@@ -10,12 +9,5 @@ describe('Time', () => {
     const component = mount(<Time />);
 
     expect(component.find('.time')).to.have.length(1);
-  });
-
-  it('should display the correct time for a fixed date', () => {
-    const date = moment().set({ year: 2016, month: 9, day: 10 });
-    const component = mount(<Time currentTime={ date } />);
-
-    expect(component.text()).to.contain('October 19, 2016');
   });
 });
